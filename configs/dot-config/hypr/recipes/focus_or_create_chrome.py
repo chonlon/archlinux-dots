951f87475a4723bf1e9154d57e71954bffb6852d
chonlon <lonelycooler.xcl@gmail.com>
2023-07-01 23:13:21 +0800

    fix class name

--- configs/dot-config/hypr/recipes/focus_or_create_chrome.py
+++ configs/dot-config/hypr/recipes/focus_or_create_chrome.py
@@ -4,7 +4,7 @@ import typer
 app = typer.Typer()
 
 cmd = "google-chrome-stable"
-title = "Google Chrome"
+title = "google chrome"
 class_name = "google-chrome"
 chrome_workspace = 5
 make_master = True
@@ -13,13 +13,13 @@ make_master = True
 if title == "" and class_name == "":
     print("title and class_name can't be both empty")
     exit(-1)
-cur_workspace = hyprlibs.get_current_workspace(0)["id"]
+cur_workspace = hyprlibs.get_current_workspace()["id"]
 windows = hyprlibs.get_windows()
 
 
 def find_in_workspace(workspace):
     for win in windows:
-        if win["title"] == title or win["class"] == class_name:
+        if win["title"].lower() == title or win["class"].lower() == class_name:
             if win["workspace"]["id"] == workspace:
                 return win
     return None
@@ -27,7 +27,7 @@ def find_in_workspace(workspace):
 
 def find_in_all():
     for win in windows:
-        if win["title"] == title or win["class"] == class_name:
+        if win["title"].lower() == title or win["class"].lower() == class_name:
             return win
     return None
 
@@ -51,7 +51,7 @@ if win is not None:
 
     if make_master:
         win_workspace = win["workspace"]["id"]
-        bwin = hyprlibs.biggest_window_in_workspace(win_workspace)
+        bwin = hyprlibs.master_window_in_workspace(win_workspace)
         if win["address"] == bwin["address"]:
             print("already master")
             if win["workspace"]["id"] == cur_workspace:

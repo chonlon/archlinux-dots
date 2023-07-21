6009e62bd5f6a3b8dc8061d4416919a634a03c7b
xu.changlong <changlong.xu@everxyz.com>
2023-07-21 19:26:14 +0800

    update recipes

--- recipes/entry.py
+++ recipes/entry.py
@@ -4,10 +4,10 @@ from pathlib import Path
 
 args = os.sys.argv
 args = args[1:]
-command = args[0]
+command = args[0] if len(args) > 0 else ''
 home_dir = os.path.expanduser('~')
 cur_dir = os.path.dirname(os.path.abspath(__file__))
-reciepes_dir = cur_dir
+reciepes_dir = Path(cur_dir)
 
 if command is None:
   print('command is empty')
@@ -15,24 +15,30 @@ if command is None:
 args = args[1:]
 
 reciepes = []
-for file in os.listdir(reciepes_dir):
-  if file.endswith('.just'):
-    reciepes.append(file)
+for directory in os.listdir(reciepes_dir):
+  if os.path.isdir(checkpath := Path(reciepes_dir) / directory):
+    checkpath = Path(checkpath)
+    #if checkpath has justfile
+    if os.path.isfile(checkpath / 'justfile'):
+      reciepes.append(checkpath)
 
 def run_recipe(recipe, args):
+  # make soft link replace to real path
+  recipe = os.path.realpath(recipe)
   if len(args) == 0:
-    cmd = f'just -f {recipe} --choose'
+    cmd = f'cd {recipe} && just --choose'
   else:
-    cmd = f'just -f {recipe} {" ".join(args)}'
+    cmd = f'cd {recipe} && just {" ".join(args)}'
 
   print(cmd)
   os.system(cmd)
 
-recipe = command + '.just'
+recipe = reciepes_dir / command
 if recipe in reciepes:
-  recipe = Path(reciepes_dir) / recipe
   run_recipe(recipe, args)
 else:
+  reciepes = [str(r).replace(str(reciepes_dir) + '/', '') for r in reciepes]
+  reciepes = [r.replace('/justfile', '') for r in reciepes]
   print(f'no such recipe: {command}')
   print(f'available recipes: {reciepes}')
   exit(1)
